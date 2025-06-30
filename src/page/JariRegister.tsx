b9362b0445f1ea3c1555bfc1cd116e2e10c54e55
lkj1313 <smm45108@gmail.com>
2025-06-30 20:39:32 +0900

    feature: jari/page 초안 완성

--- src/page/JariRegister.tsx
+++ src/page/JariRegister.tsx
@@ -1,5 +1,28 @@
+import { Input } from "@/shared/ui/input/Input";
+
+import { Search } from "lucide-react";
 const JariRegisterPage = () => {
-  return <div>JariRegisterPage</div>;
+  return (
+    <section className=" flex flex-col items-center h-full p-20 px-4">
+      <div className="text-center space-y-6">
+        <h1 className="text-xl  mb:text-3xl font-bold">
+          메랜자리 자리 등록 페이지입니다.
+        </h1>
+        <p className="text-muted-foreground text-sm sm:text-base">
+          원하는 사냥터를 검색해주세요.
+        </p>
+        <div className="max-w-sm w-full mx-auto relative">
+          <Input
+            placeholder="예: 구름공원, 죽은나무숲2, 망가진 용의 둥지..."
+            className="h-12 pr-10"
+          />
+          <div className="absolute right-0 top-0 h-full w-10 flex items-center justify-center z-10">
+            <Search className="w-5 h-5 text-muted-foreground cursor-pointer" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default JariRegisterPage;
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -7,16 +7,19 @@ import {
   HeaderTradeButton,
   useProfileMenu,
 } from "./index";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
   const user = useUser();
   const { menuOpen, toggleMenu, closeMenu, dropdownRef, handleDiscordLogin } =
     useProfileMenu();
-
+  const location = useLocation();
+  const pathname = location.pathname;
+  const hideSearchBar = /^\/jari\/register(\/.*)?$/.test(pathname);
   return (
     <header className="sticky top-0 z-50 w-full bg-neutral-900 flex items-center justify-between border-b py-6 px-4 sm:px-4 md:px-6 lg:px-8 xl:px-12 border-neutral-800">
       <HeaderLogo />
-      <HeaderSearchBar />
+      {!hideSearchBar && <HeaderSearchBar />}
       <div className="flex gap-3">
         {user && <HeaderTradeButton />}
         <div ref={dropdownRef} className="relative">
