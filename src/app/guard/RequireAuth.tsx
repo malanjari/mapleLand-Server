0e3d8f5368c4bc252b94759a2350e7e73d6cfb45
lkj1313 <smm45108@gmail.com>
2025-07-02 17:59:50 +0900

    feat: 로그인 안됐을시 guard 설정 및 toast 설치 및 실행

--- src/app/guard/RequireAuth.tsx
+++ src/app/guard/RequireAuth.tsx
@@ -1,19 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "@/entity/user/hooks/useUser";
+import { toast } from "@/shared/hooks/use-toast";
 
 export const RequireAuth = ({ children }: { children: React.ReactNode }) => {
   const user = useUser();
   const navigate = useNavigate();
+  const hasRedirected = useRef(false);
 
   useEffect(() => {
     if (!user) {
-      alert("로그인이 필요합니다.");
+      hasRedirected.current = true;
+
+      toast({
+        title: "로그인이 필요합니다.",
+        description: "이 페이지에 접근하려면 로그인이 필요해요.",
+        variant: "destructive",
+        duration: 4000,
+      });
       navigate("/", { replace: true });
     }
   }, [user, navigate]);
 
-  // user가 없으면 아직 redirect 중이라 아무것도 안 보여줌
   if (!user) return null;
 
   return <>{children}</>;
--- src/shared/ui/layouts/AppLayout.tsx
+++ src/shared/ui/layouts/AppLayout.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 
 import { useAuthActions } from "@/entity/user/hooks/useUser";
 import ScrollToTop from "@/app/ScrollToTop";
+import { Toaster } from "../toast/toaster";
 const AppLayout = () => {
   const { initialize } = useAuthActions();
 
@@ -15,6 +16,7 @@ const AppLayout = () => {
     <div className="flex flex-col flex-grow px-4 sm:px-6 md:px-10 lg:px-20 xl:px-28 w-full h-dvh mx-auto  ">
       <Header />
       <ScrollToTop />
+      <Toaster />
       <main className="max-w-[1440px] flex-col items-center w-full mx-auto flex-grow  py-6 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-16 ">
         <Outlet />
       </main>
