e1bbfaefa9268b4c8dd484e184d8b1f6aabe1ab2
MurHyun2 <eddy152264@gmail.com>
2025-07-16 13:53:15 +0900

    refactor: backend 폴더 병합

--- backend/src/main/java/org/mapleLand/maplelanbackserver/dto/MapPopularityDto.java
+++
@@ -1,3 +0,0 @@
-package org.mapleLand.maplelanbackserver.dto;
-
-public record MapPopularityDto(String mapName, int registerCount,String area,String monsterImageUrl,int avgPrice) { }
--- backend/src/main/java/org/mapleLand/maplelanbackserver/dto/UserDetailResponseDto.java
+++
@@ -1,9 +0,0 @@
-package org.mapleLand.maplelanbackserver.dto;
-
-import java.util.List;
-
-public record UserDetailResponseDto(
-        UserInfoDto userInfo,
-        List<UserMapRegistrationDto> mapRegistrations
-) {
-} 
