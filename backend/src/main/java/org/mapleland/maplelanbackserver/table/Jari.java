cb2a4aabc0ba10638c9ed86ecb02ba61d5e006ff
golden-hamster <isack19999@gmail.com>
2025-07-20 23:24:48 +0900

    feat(db): `bumpedTime(끌올 시간)`, `isBumped(끌올 여부)` 를 필드에 추가 (#37)

--- backend/src/main/java/org/mapleland/maplelanbackserver/table/Jari.java
+++ backend/src/main/java/org/mapleland/maplelanbackserver/table/Jari.java
@@ -19,34 +19,60 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 public class Jari {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userMapId; //PK userMapId 사용자가 등록한 자리 pk
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user; // user 테이블 FK
+
     @Column(length = 30)
     private String mapName; // 맵 네임 : 남겨진 용의 둥지 , 죽은 용의 둥지 등..
+
     private String serverColor; // 서버 색깔 : 초채 노채 빨채등
+
     private int price; // 가격
+
     @Enumerated(EnumType.STRING)
     private TradeType tradeType; // BUY SELL
+
     private Boolean negotiationOption; // 흥정 여부 : 가능 , 불가능
+
     @Enumerated(EnumType.STRING)
     @Column(length = 30)  //
     private Region area; // 지역명 Leafre Elnath Aquarium , LudusLake
+
     @CreationTimestamp
     @Column(updatable = false, columnDefinition = "DATETIME(0)")
     private LocalDateTime createTime; // 만든 날짜
+
+    @CreationTimestamp
+    @Column(columnDefinition = "DATETIME(0)")
+    private LocalDateTime bumpedTime; // 끌올 된 날짜
+
     @Column(length = 60)
     private String comment; // 유저 코멘트
+
     private String monsterImageUrl; // 몬스터 이미지
+
     private Boolean isCompleted; // 거래 완료 여부
+
+    private Boolean isBumped; // 끌올 여부
+
     @OneToMany(mappedBy = "jari", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Report> reports;
 
     public Jari() {
 
+    }
+
+    public void bump() {
+        this.bumpedTime = LocalDateTime.now();
+        this.isBumped = true;
+    }
+
+    public boolean validateOwner(Integer userId) {
+        return this.user.getUserId().equals(userId);
     }
 }
