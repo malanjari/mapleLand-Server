0ff1500ee98ae9cc19e777bd0829403614c51b75
lkj1313 <smm45108@gmail.com>
2025-07-25 22:57:32 +0900

    fix: 검색어와 무관한 맵이 추천되는 문제 수정

--- frontend/src/shared/lib/isSubsequence.ts
+++
@@ -1,8 +0,0 @@
-export const isSubsequence = (small: string, big: string) => {
-  let i = 0;
-  for (let j = 0; j < big.length; j++) {
-    if (small[i] === big[j]) i++;
-    if (i === small.length) return true;
-  }
-  return false;
-};
