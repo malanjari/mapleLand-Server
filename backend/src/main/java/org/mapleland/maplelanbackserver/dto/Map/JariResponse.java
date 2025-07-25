cf21ceae09fef9d97ff25a7d86280a988d02d2e9
golden-hamster <isack19999@gmail.com>
2025-07-25 23:47:05 +0900

    feat: 모든 자리 거래 조회 API 구현 (#128)

--- backend/src/main/java/org/mapleland/maplelanbackserver/dto/Map/JariResponse.java
+++ backend/src/main/java/org/mapleland/maplelanbackserver/dto/Map/JariResponse.java
@@ -3,6 +3,7 @@ package org.mapleland.maplelanbackserver.dto.Map;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.mapleland.maplelanbackserver.enumType.Region;
 import org.mapleland.maplelanbackserver.enumType.TradeType;
+import org.mapleland.maplelanbackserver.table.Jari;
 
 import java.time.LocalDateTime;
 @Schema(description = "맵 정보 Dto")
@@ -22,4 +23,25 @@ public record JariResponse(
         int userId,
         String discordId,
         boolean isCompleted
-) {}
+) {
+
+    public static JariResponse from(Jari jari) {
+        return new JariResponse(
+                jari.getUserMapId(),
+                jari.getMapName(),
+                jari.getServerColor(),
+                jari.getPrice(),
+                jari.getTradeType(),
+                jari.getNegotiationOption(),
+                jari.getArea(),
+                jari.getCreateTime(),
+                jari.getComment(),
+                jari.getMonsterImageUrl(),
+                jari.getUser().getGlobalName(),
+                jari.getUser().getImage(),
+                jari.getUser().getUserId(),
+                jari.getUser().getDiscordId(),
+                jari.getIsCompleted()
+        );
+    }
+}
