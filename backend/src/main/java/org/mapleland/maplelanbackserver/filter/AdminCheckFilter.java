ab0d1ff587979f5b7119c2a1419a0aab27a7949e
우희섭 <132733651+dnjswns1992@users.noreply.github.com>
2025-07-26 18:49:35 +0900

    AdminCheckFilter.java

--- backend/src/main/java/org/mapleland/maplelanbackserver/filter/AdminCheckFilter.java
+++ backend/src/main/java/org/mapleland/maplelanbackserver/filter/AdminCheckFilter.java
@@ -2,13 +2,14 @@ package org.mapleland.maplelanbackserver.filter;
 
 public class AdminCheckFilter {
 
-
     public static String adminCheckFilter(String discordId) {
 
-
-        if (discordId.equals("821213405360029698") || discordId.equals("1147804753691148318") ||
-                discordId.equals("272022728955920384") || discordId.equals("423159863141728256") ||
-                discordId.equals("323827890150047744" || discordId.equals("1202998458139803698"))) {
+        if (discordId.equals("821213405360029698") ||
+            discordId.equals("1147804753691148318") ||
+            discordId.equals("272022728955920384") ||
+            discordId.equals("423159863141728256") ||
+            discordId.equals("323827890150047744") ||
+            discordId.equals("1202998458139803698")) {
 
             return "ROLE_ADMIN";
         }
